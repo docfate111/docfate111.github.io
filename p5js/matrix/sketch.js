96889c3c3e1b3e91b04e9ddb399dc60b5c226cb8
Williams_Thelford <tdwilliamsiv@gmail.com>
2019-12-15 22:12:56 -0700

    Add files via upload

--- p5js/matrix/sketch.js
+++ p5js/matrix/sketch.js
@@ -4,7 +4,7 @@ function setup() {
     window.innerHeight
   );
   l=[];
-  for(var i=0; i<random(200, 800); i++){
+  for(var i=0; i<random(200, 900); i++){
         for(var j=0; j<width; j+=80){
           l.push(new symbol(j));
         }
@@ -13,15 +13,13 @@ function setup() {
 function draw() {
   background(0);
   l.forEach(function(line){
-    line.fall();
+    line.fall(abs(mouseX));
     line.show();
   });
 }
 function symbol(x){
   this.x=x;
   this.y=random(-height*3, -height);
-  this.speed=abs(mouseY)*40
-  this.a=-5;
   this.randomSymbol=function(){
     return String.fromCharCode(0x30A0+round(random(0, 96)));
   };
@@ -30,14 +28,10 @@ function symbol(x){
     textSize(50);
     text(this.randomSymbol(), this.x, this.y);
   };
-  this.fall=function(){
-    this.y+=this.speed;
-    this.speed+=this.a;
-    /*if(this.speed<=5){
-      this.speed+=random(80, 300);
-    }*/
+  this.fall=function(x){
+    this.y+=x;
     if(this.y>height){
-      this.y=random(0, height/2);
+      this.y=random(0, height);
     }
   };
 }
